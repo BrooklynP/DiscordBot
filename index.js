d627d2aac940896d80702da1be299ee51abfeacf
BrooklynP <Brooklynpedley@gmail.com>
2023-03-07 17:22:14 +0000

    Refactor

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ import config from "./config.json" assert { type: "json" };
 import * as welcomeMessages from "./welcomeMessages.js"
 import * as safetyMessages from "./safetyMessages.js"
 import * as ruleMessages from "./ruleMessages.js"
-import *  as FAQMessages from "./FAQMessages.js" 
-import *  as NFTInfo from "./NFTInfoMessages.js" 
-import http from 'http'
+import *  as FAQMessages from "./FAQMessages.js"
+import *  as NFTInfo from "./NFTInfoMessages.js"
 import fetch from 'node-fetch'
 
 const client = new Discord.Client({
@@ -14,178 +13,183 @@ const client = new Discord.Client({
         Discord.GatewayIntentBits.GuildMessages,
         Discord.GatewayIntentBits.MessageContent,
     ],
-    partials: 
-    ["GUILD_MEMBER"]
+    partials:
+        ["GUILD_MEMBER"]
 });
 client.login(config.BOT_TOKEN);
 
-const postData = JSON.stringify( { "condition": { "display_name": "BAT x Adam Ape for Brave" } })
+const postData = JSON.stringify({ "condition": { "display_name": "BAT x Adam Ape for Brave" } })
 const options = {
-    // hostname: 'https://beta.api.solanalysis.com',
-    // path: '/rest/get-project-stat-by-name',
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json',
-    //   'Content-Length': Buffer.byteLength(postData),
-      'Authorization': config.HYPERSPACE_AUTH_TOKEN
+        'Content-Type': 'application/json',
+        'Authorization': config.HYPERSPACE_AUTH_TOKEN
     },
     body: postData
-  };
+};
 
 const AvailableFAQs = {
     NFTSaleStats: "!nft_sale",
-    NFTStats:  "!nft_stats",
-    SecondaryMarkets:  "!nft_secondary",
-    HolderVerification:  "!nft_verify",
-    NFTBlog:  "!nft_blog",
-    NFTUtility:  "!nft_utility",
-    SageDetails:  "!nft_sage",
-    POAP:  "!faq_poap",
-    TIPCC:  "!faq_tipcc"
+    NFTStats: "!nft_stats",
+    SecondaryMarkets: "!nft_secondary",
+    HolderVerification: "!nft_verify",
+    NFTBlog: "!nft_blog",
+    NFTUtility: "!nft_utility",
+    SageDetails: "!nft_sage",
+    POAP: "!faq_poap",
+    TIPCC: "!faq_tipcc"
 }
 
-async function GetProjectStats(){
+async function GetProjectStats() {
     const response = await fetch('https://beta.api.solanalysis.com/rest/get-project-stat-by-name', options)
     return response.json();
 }
 
-function UpdateProjectFloorPrice(){
-    GetProjectStats().then((stats=>{
-        // client.user.setPresence({activities:[{name: 'Floor Price: ' + stats.project_stats[0].floor_price + 'SOL'}]})
+function UpdateProjectFloorPrice() {
+    GetProjectStats().then((stats => {
         client.user.setActivity('Floor Price: ' + stats.project_stats[0].floor_price + 'SOL')
         console.log("Set FP to " + stats.project_stats[0].floor_price + "SOL")
     }))
 }
 
-client.on('ready',  (clientReady) =>{
-    UpdateProjectFloorPrice()
-    setInterval(() => {
-        UpdateProjectFloorPrice()
-    }, 900000);
-    // const request = http.request(options, (response) => {
-    //     response.setEncoding('utf8');
-    
-    //     response.on('data', (chunk) => {
-    //       data += chunk;
-    //     });
-    
-    //     response.on('end', () => {
-    //       console.log(data);
-    //     });
-    //   });
-    // request.on('error', (error) => {
-    //     console.error(error);
-    // });
-    // request.write(postData);
-    // request.end();
-
-    // console.log("running")
+function giveUsersRoleFromReaction(ChannelID, RoleID, MessageID, EmojiReactionID, ServerID) {
     // let batbromessageID = '1035653394217582603'
     // let batbrochannelID = '846472323250257951'
     // let bravelistroleID = '1034373289222610946'
     // let batpepereactionID = '839871482535608380'
     // let batbrigadeserverID = '839283762843484192'
-    // client.channels.fetch(batbrochannelID).then(channel => {
-    //     channel.messages.fetch(batbromessageID).then(message =>{
-    //         message.reactions.resolve(batpepereactionID).users.fetch().then(userList=>{
-    //             // console.log(userList);
-    //             client.guilds.fetch(batbrigadeserverID).then(guild =>{
-    //                 const roleToGive = guild.roles.cache.get(bravelistroleID)
-    //                 userList.forEach(userListUser => {
-    //                     console.log(userListUser.username + '#' + userListUser.discriminator)
-    //                     // console.log("found user", userListUser.id);
-    //                     // guild.members.fetch(userListUser.id).then(MemberUser =>{                      
-    //                     //     MemberUser.roles.add(roleToGive);
-    //                     //     console.log("role given", userListUser.id);
-    //                     // })
-                        
-    //                 });
-    //         })
 
-    //         })
-    //     })
-    // })
-    // client.channels.fetch(config.RULE_CHANNEL_ID).then(channel => 
-    // {
-    //     channel.send({
-    //         embeds: ruleMessages.ruleEmbed()
-    //     }).then(sentMessage =>  {
+    client.channels.fetch(ChannelID).then(channel => {
+        channel.messages.fetch(MessageID).then(message => {
+            message.reactions.resolve(EmojiReactionID).users.fetch().then(userList => {
+                client.guilds.fetch(ServerID).then(guild => {
+                    const roleToGive = guild.roles.cache.get(RoleID)
+                    userList.forEach(userListUser => {
+                        console.log(userListUser.username + '#' + userListUser.discriminator)
+                        guild.members.fetch(userListUser.id).then(MemberUser => {
+                            MemberUser.roles.add(roleToGive);
+                            console.log("role given", userListUser.id);
+                        })
 
-    //     })
-    // });
+                    });
+                })
 
-    // client.channels.fetch(config.NFT_INFO_CHANNEL_ID).then(channel => 
-    //     {
-    //         channel.messages.fetch('1060316513048985640').then(message =>{
-    //             message.edit({
-    //                 embeds: NFTInfoEmbed()
-    //             })
-    //         })
-    //         // channel.send({
-    //         //     embeds: NFTInfoEmbed()
-    //         // }).then(sentMessage =>  {
-    
-    //         // })
-    //     });
-        // const MessageButtonCollector = channel.createMessageComponentCollector()
-        // console.log("created button hook")
-        // MessageButtonCollector.on('collect', interaction => {
-        //     console.log("button pressed");
-        //     console.log(interaction)
-        //     const member = interaction.guild.members.cache.get(interaction.user.id);
-        //     if(interaction.message.id === welcomeMessageID) {
-        //         client.users.fetch(interaction.user.id).then(user =>{
-        //             console.log(user);
-        //             //const roleToGive = interaction.guild.roles.cache.find(role => role.name === "Member")
-        //             const roleToGive = interaction.guild.roles.cache.get(config.MEMBER_ROLE_ID)
-        //             member.roles.add(roleToGive)
-        //             console.log("added role");
-        //             interaction.reply({ content: 'Added Member Role', ephemeral: true });
-        //         })
-        //     }
-        // })
-        // console.log("created button hook event");
-    // })
+            })
+        })
+    })
+}
 
-        // const safetyChannel = client.channels.fetch(config.SAFETY_CHANNEL_ID).then (channel => 
-        //     {
-        //         channel.send({embeds: safetyMessages.safetyEmbed1()})
-    
-        //     })
-        // const rulesChannel = client.channels.fetch(config.RULE_CHANNEL_ID).then (channel => 
-        //     {
-        //         channel.send({embeds: ruleMessages.ruleEmbed()})
-    
-        //     })
-        // client.channels.fetch(config.WELCOME_CHANNEL_ID).then (channel => 
-        //     {
-        //         channel.send({embeds: welcomeEmbed.welcomeEmbed()})
-    
-        //     })
+function createVerificationMessage() {
+    const MessageButtonCollector = channel.createMessageComponentCollector()
+    console.log("created button hook")
+    MessageButtonCollector.on('collect', interaction => {
+        console.log("button pressed");
+        console.log(interaction)
+        const member = interaction.guild.members.cache.get(interaction.user.id);
+        if (interaction.message.id === welcomeMessageID) {
+            client.users.fetch(interaction.user.id).then(user => {
+                console.log(user);
+                //const roleToGive = interaction.guild.roles.cache.find(role => role.name === "Member")
+                const roleToGive = interaction.guild.roles.cache.get(config.MEMBER_ROLE_ID)
+                member.roles.add(roleToGive)
+                console.log("added role");
+                interaction.reply({ content: 'Added Member Role', ephemeral: true });
+            })
+        }
+    })
+    console.log("created button hook event");
+}
 
-            //1023520486132563988 server makeover channel
+function updateWelcomeMessage() {
+    //1023520486132563988 server makeover channel
+    client.channels.fetch(config.WELCOME_CHANNEL_ID).then(channel => {
+        channel.messages.fetch('1035236461924077588').then(message => {
+            message.edit({ embeds: welcomeMessages.welcomeEmbed() })
+        })
+    });
+}
+function updateImportantInfoChannel() {
+    client.channels.fetch(config.NFT_INFO_CHANNEL_ID).then(channel => {
+        channel.messages.fetch('1060316513048985640').then(message => {
+            message.edit({ embeds: NFTInfoEmbed() })
+        })
+    });
+}
 
-        // client.channels.fetch(config.WELCOME_CHANNEL_ID).then(channel => 
-        // {
-        //     channel.messages.fetch('1035236461924077588').then(message =>{
-        //         message.edit({embeds: welcomeMessages.welcomeEmbed()})
-        //     })
-        // });
+function HandleFAQCommands(messageContent) {
+    let embeds = []
+    switch (messageContent) {
+        case "!faq_help":
+            let FAQDescription = ""
+            for (const [key, faq] of Object.entries(AvailableFAQs)) {
+                FAQDescription += faq
+                FAQDescription += "\n"
+            }
+            embeds = [new Discord.EmbedBuilder().setTitle("List of FAQ commands").setDescription(FAQDescription).setImage('https://cdn.discordapp.com/attachments/1029463141983211593/1048992290674323476/rules2.png').setColor(0x9E1F63)]
+            break;
+        case AvailableFAQs.NFTSaleStats:
+            embeds = NFTInfo.getOriginalSaleInfoEmbed()
+            break;
+        case AvailableFAQs.NFTStats:
+            embeds = NFTInfo.getNFTStatsEmbed()
+            break;
+        case AvailableFAQs.SecondaryMarkets:
+            embeds = NFTInfo.getSecondaryInfoEmbed()
+            break;
+        case AvailableFAQs.HolderVerification:
+            embeds = NFTInfo.getVerificationGuideEmbed()
+            break;
+        case AvailableFAQs.NFTBlog:
+            embeds = NFTInfo.getBlogPostEmbed()
+            break;
+        case AvailableFAQs.NFTUtility:
+            embeds = NFTInfo.getUtilityEmbed()
+            break;
+        case AvailableFAQs.SageDetails:
+            embeds = NFTInfo.getSageInfoEmbed()
+            break;
+        case AvailableFAQs.POAP:
+            embeds = FAQMessages.getPOAPFAQ()
+            break;
+        case AvailableFAQs.TIPCC:
+            embeds = FAQMessages.getTipCCFAQ()
+            break;
+        default:
+            embeds = []
+            break;
+    }
+    try {
+        if (embeds.length > 0) {
+            client.channels.fetch(channelID).then(channel => {
+                channel.send({ embeds: embeds })
+            })
+        }
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+client.on('ready', (clientReady) => {
+    UpdateProjectFloorPrice()
+    setInterval(() => {
+        UpdateProjectFloorPrice()
+    }, 900000);
 })
 
-client.on('messageCreate', message =>{
+client.on('messageCreate', message => {
     const messageContent = message.content.toLowerCase()
     const channelID = message.channelId
     const senderID = message.author.id
 
-    if (senderID == 1029393319924609115) {
+    if (senderID == 1029393319924609115n) {
         return
     }
     if (messageContent.includes(" gm ") || messageContent.startsWith("gm") || messageContent.endsWith("gm")) {
         message.react('👋')
         message.react('845061306259734578')
     }
-    if (messageContent.includes("bat chan") || messageContent.includes("bat-chan") ) {
+    if (messageContent.includes("bat chan") || messageContent.includes("bat-chan")) {
         message.react('👀')
     }
     if (messageContent.includes(" dru ") || messageContent.startsWith("dru") || messageContent.endsWith("dru")) {
@@ -203,7 +207,7 @@ client.on('messageCreate', message =>{
     if (messageContent.includes(" gn ") || messageContent.startsWith("gn") || messageContent.endsWith("gn")) {
         message.react('845061305874513931')
     }
-    if (messageContent.includes(" wen ") || messageContent.startsWith("wen") || messageContent.endsWith("wen")){
+    if (messageContent.includes(" wen ") || messageContent.startsWith("wen") || messageContent.endsWith("wen")) {
         message.reply('<:wen:1049723300739297310>')
     }//<:emoji name:emoji id> emoji_name:emoji_id
     if (messageContent === "ping") {
@@ -211,58 +215,8 @@ client.on('messageCreate', message =>{
         message.reply('pong')
     }
 
-    if(messageContent.startsWith("!")){
-        let embeds = []
-        switch(messageContent){
-            case "!faq_help":
-                let FAQDescription = ""
-                for(const [key,faq] of Object.entries(AvailableFAQs)){
-                    FAQDescription += faq
-                    FAQDescription += "\n"
-                }
-                embeds = [new Discord.EmbedBuilder().setTitle("List of FAQ commands").setDescription(FAQDescription).setImage('https://cdn.discordapp.com/attachments/1029463141983211593/1048992290674323476/rules2.png').setColor(0x9E1F63)]
-                break;
-            case AvailableFAQs.NFTSaleStats:
-                embeds = NFTInfo.getOriginalSaleInfoEmbed()
-                break;
-            case AvailableFAQs.NFTStats:
-                embeds = NFTInfo.getNFTStatsEmbed()
-                break;
-            case AvailableFAQs.SecondaryMarkets:
-                embeds = NFTInfo.getSecondaryInfoEmbed()
-                break;
-            case AvailableFAQs.HolderVerification:
-                embeds = NFTInfo.getVerificationGuideEmbed()
-                break;
-            case AvailableFAQs.NFTBlog:
-                embeds = NFTInfo.getBlogPostEmbed()
-                break;
-            case AvailableFAQs.NFTUtility:
-                embeds = NFTInfo.getUtilityEmbed()
-                break;
-            case AvailableFAQs.SageDetails:
-                embeds = NFTInfo.getSageInfoEmbed()
-                break;
-            case AvailableFAQs.POAP:
-                embeds = FAQMessages.getPOAPFAQ()
-                break;
-            case AvailableFAQs.TIPCC:
-                embeds = FAQMessages.getTipCCFAQ()
-                break;
-            default:
-                embeds = []
-                break;
-        }
-        try {
-            if (embeds.length > 0){
-                client.channels.fetch(channelID).then (channel => {
-                    channel.send({embeds: embeds})
-                })
-            }
-        }
-        catch (error) {
-            console.log(error)
-        }
+    if (messageContent.startsWith("!")) {
+        HandleFAQCommands(messageContent)
     }
 })
 
